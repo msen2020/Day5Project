b084af340dad6d77f3716f10c1ffa89174c2bb50
EU3 Batch GitHup <msen2020@hotmail.com>
2020-10-15 00:02:08 +0200

    Awesome Feature2 more added

--- src/test/java/Feature2.java
+++ src/test/java/Feature2.java
@@ -2,7 +2,7 @@ public class Feature2 {
     public static void main(String[] args) {
         System.out.println("Working on Feature2");
         System.out.println("Working on Feature2 more");
-        System.out.println("Working on Feature2 again");
+        System.out.println("Working on Feature 2 again");
 
     }
 }
