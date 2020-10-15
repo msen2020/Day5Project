e6e5742c767d1a17f48e627fdf45a7b8dbad9307
EU3 Batch GitHup <msen2020@hotmail.com>
2020-10-15 22:41:25 +0200

    adding new line of comment locally, not pushed yet

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
 
-        System.out.println("Sill really My Coworker added Awesome Hello"); //We are simuliating the situatyion my coworker made this change !!!
-        
+        System.out.println("Sill really My Coworker added Awesome Hello"); //We are simulating the situation my coworker made this change !!!
+        // adding some local change
     }  
 }
