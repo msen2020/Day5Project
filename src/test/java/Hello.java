f488840a4ee3dfe620d356d0488b840d7e031608
EU3 Batch GitHup <msen2020@hotmail.com>
2020-10-15 22:15:09 +0200

    added some comment on a new line

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,7 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
 
-        System.out.println("Sill really Awesome Hello");
-        //add one more line on your local and commit
+        System.out.println("This is my own Hello Hello"); // this is my local Hello, and committing
     }
 }
